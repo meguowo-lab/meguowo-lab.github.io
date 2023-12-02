6fec2c4f2dcc1ed69cb3aa303c5c9c5662c7d235
meguowo-lab <file.name.infected@gmail.com>
2023-12-02 21:06:50 +0700

    biggest shit

--- abc.js
+++ abc.js
@@ -1,5 +1,24 @@
 const toggle = document.querySelector(".menu-toggle-div input")
 const hamburgerMenu = document.querySelector(".center-end-wraper")
+const likeButtonCircle = document.getElementById("like-btn-circle")
 toggle.addEventListener("click", function(){
     hamburgerMenu.classList.toggle("open")
 })
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+        if (entry.isIntersecting){
+            entry.target.classList.add("show-post")
+        }
+        else{
+            entry.target.classList.remove("show-post")
+        }
+    })
+})
+
+const obsPosts = document.querySelectorAll(".blog-card")
+obsPosts.forEach((el) => observer.observe(el))
+
+function hello(){
+    likeButtonCircle.classList.toggle("like-pressed")
+}
